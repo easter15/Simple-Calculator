1cb433633e5053533076e2996e05751f24ea73c1
Tony Easter <58912691+easter15@users.noreply.github.com>
2023-01-16 21:33:00 +0000

    Changed order of inputs

--- Simple Calculator/Program.cs
+++ Simple Calculator/Program.cs
@@ -1,59 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Runtime.CompilerServices;
 
-
-using System.Runtime.CompilerServices;
 
 do
 {
 
+
     double num1 = 0;
     double num2 = 0;
     double result = 0;
+    string symbol = "";
 
-
-
-    Console.WriteLine("-----------------");
+    Console.WriteLine("*****************\n");
     Console.WriteLine("Simple Calculator");
-    Console.WriteLine("-----------------");
+    Console.WriteLine("\n*****************\n");
 
-    Console.Write("Enter your first number: ");
+    Console.Write("Please enter your first number: ");
     num1 = Convert.ToDouble(Console.ReadLine());
 
-    Console.Write("Enter your second number: ");
+    Console.WriteLine("\nPlease enter a mathematical symbol: ");
+    Console.WriteLine("\t+ Add");
+    Console.WriteLine("\t- Subtract");
+    Console.WriteLine("\t* Multiply");
+    Console.WriteLine("\t/ Divide");
+    symbol = Console.ReadLine();
+
+    Console.Write("\nPlease enter your second number: ");
     num2 = Convert.ToDouble(Console.ReadLine());
 
-    Console.WriteLine("Enter one of the mathematical symbols: ");
-    Console.WriteLine("\t+ : Add");
-    Console.WriteLine("\t- : Subtract");
-    Console.WriteLine("\t* : Multiply");
-    Console.WriteLine("\t/ : Divide");
-  
-    
-    switch (Console.ReadLine())
+
+    switch (symbol)
     {
         case "+":
             result = num1 + num2;
-            Console.WriteLine($"Your result: {num1} + {num2} = " + result);
+            Console.WriteLine($"\nYour result: {num1} + {num2} = " + result);
             break;
         case "-":
             result = num1 - num2;
-            Console.WriteLine($"Your result: {num1} - {num2} = " + result);
+            Console.WriteLine($"\nYour result: {num1} - {num2} = " + result);
             break;
         case "*":
             result = num1 * num2;
-            Console.WriteLine($"Your result: {num1} * {num2} = " + result);
+            Console.WriteLine($"\nYour result: {num1} * {num2} = " + result);
             break;
         case "/":
             result = num1 / num2;
-            Console.WriteLine($"Your result: {num1} / {num2} = " + result);
+            Console.WriteLine($"\nYour result: {num1} / {num2} = " + result);
             break;
         default:
             Console.WriteLine("That was not a valid option");
-                break;
+            break;
     }
     Console.Write("Woud you like to do another calculation? (Y = Yes, N = No): ");
 
 } while (Console.ReadLine().ToUpper() == "Y");
 
 Console.WriteLine("\nThank you for using my calculator.");
- Console.ReadKey(); 
